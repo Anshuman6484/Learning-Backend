5d78f7495e6c7b3a015d3419c8b13c7e52dee90e
Kumar Anshuman <kanshuman6484@gmail.com>
2025-03-09 14:47:55 +0530

    added all files

--- 01-Node&NPM/index.js
+++ 01-Node&NPM/index.js
@@ -1,26 +1,26 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const slugify = require('slugify')
 
 const replaceTemplate = require('./modules/replaceTemplate')
 
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const tempProducts = fs.readFileSync(
-  `${__dirname}/templates/template-products.html`,
-  'utf-8'
-)
+const tempProducts = fs.readFileSync(`${__dirname}/templates/template-products.html`, 'utf-8')
 
 const tempProductDetails = fs.readFileSync(
   `${__dirname}/templates/template-productDetails.html`,
   'utf-8'
 )
 
-const tempCards = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-)
+const tempCards = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+
+const slugs = dataObj.map((item) => slugify(item.name, { lower: true }))
+console.log(slugs)
+
+// console.log(slugify('Hello World!', { lower: true }))
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true)
@@ -29,9 +29,7 @@ const server = http.createServer((req, res) => {
   if (pathname == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' })
 
-    const prodHtml = dataObj
-      .map((item) => replaceTemplate(tempCards, item))
-      .join('')
+    const prodHtml = dataObj.map((item) => replaceTemplate(tempCards, item)).join('')
     const output = tempProducts.replace(/{#PRODUCTS_CARD#}/g, prodHtml)
 
     res.end(output)
@@ -59,5 +57,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(5000, () => {
-  console.log('Server is listening on port 5000')
+  console.log('Listening for requests on port 5000...')
 })
