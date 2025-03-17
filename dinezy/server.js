3f9fd82aba403b85dac9cceb8b9d7e0b5d2bafa5
Kumar Anshuman <kanshuman6484@gmail.com>
2025-03-17 16:13:06 +0530

    added and updated files

--- dinezy/server.js
+++ dinezy/server.js
@@ -1,10 +1,17 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 
 const app = require('./app')
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+)
+
+mongoose.connect(DB, {}).then((obj) => console.log('DB connection successful!'))
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`server is listening to requests on port ${port}...`)
 })
-
